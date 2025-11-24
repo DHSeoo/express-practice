5cbad8a8920593640fe17e1cba486819caa451ef
DHSeo <ehdgus1358@ajou.ac.kr>
2025-11-24 22:21:21 +0900

    practice (2/2)

--- app.js
+++ app.js
@@ -1,19 +1,28 @@
-const express = require('express')
-const logger = require('morgan')
+var express = require('express');
+const logger = require('morgan');
 const axios = require('axios'); // npm install axios --save
-//const list = require('/.data'); // X
+const list = require('./data');
+const firebase = require('./firebase')
 
-// npm install morgan --save
 const app = express()
 const port = 3000
 
 app.use(express.json())
-app.use(express.urlencoded({extended : true}))
+app.use(express.urlencoded({'extended' : true}))
 app.use(logger('dev'))
+app.use(express.static('public')); // html, image 등 정적파일 제공 폴더 지정
 
+
+app.get('/',(req,res)=> {
+    res.sendFile('index.html');
+})
+
+
+//curl localhost:3000/user/tommy
 app.get('/user/:id',(req,res)=> {
     res.send(`User id is ${req.params.id}`); // Or res.send('User id is ' + req.params.id);
 })
+
 
 app.get('/user',(req,res)=> {
     res.send(`User id is ${req.query.id}`);
@@ -25,20 +34,27 @@ app.post('/user',(req,res)=> {
     res.send(req.body);
 })
 
-app.get('/',(req,res)=> {
-    res.send('Hello World!')
+app.get('/music_list', (req,res) => {
+    res.json(list);
 })
 
-app.get('/musicSearch/:term', async (req,res)=> {
-    const params = {
-        term : req.params.term,
-        entity : "album",
+
+app.get('/likes', async (req, res) => {
+    const db = firebase.firestore();
+    const snapshot = await db.collection('likes').get().catch(e=> console.log(e));
+    let results = [];
+    if (snapshot.empty) {
+        console.log("No result");
+        res.json([]);
+        return;
+    } else {
+        snapshot.forEach(doc => {
+        results.push(doc.data())
+        //console.log(doc.id, '=>', doc.data());
+    })
+    res.json(results);
     }
-    var response = await axios.get('https://itunes.apple.com/search', {params:params});
-    console.log(response.data);
-    res.json(response.data);
 })
-
-app.listen(port,()=>{
-    console.log('Example app listening at http://localhost:${port}')
-})
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
