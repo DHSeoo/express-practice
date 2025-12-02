f5fe8ec4058913ad07a9277f312db4963d558010
DHSeo <ehdgus1358@ajou.ac.kr>
2025-12-02 15:16:37 +0900

    Add js file

---
+++ data.js
@@ -0,0 +1,6 @@
+module.exports = [
+    { id: 1, title: "Song A", artist: "Artist 1" },
+    { id: 2, title: "Song B", artist: "Artist 2" },
+    { id: 3, title: "Song C", artist: "Artist 3" }
+  ];
+  
